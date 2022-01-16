ca371d3c6830e0c48b7c8ab51fde219c89a2c16f
Posccis <lkzq33@gmail.com>
2022-01-16 20:26:17 -0300

    feat(main): added the 'getById' and 'getByName' in the Aluno controller

--- SmartSchoolAPI/Controllers/AlunoController.cs
+++ SmartSchoolAPI/Controllers/AlunoController.cs
@@ -2,8 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-//using SmartSchoolAPI.Models;
+using SmartSchoolAPI.Models;
 
 namespace SmartSchoolAPI.Controllers
 {
@@ -11,13 +12,54 @@ namespace SmartSchoolAPI.Controllers
     [ApiController]
     public class AlunoController : ControllerBase
     {
+        public List<Aluno> Alunos = new List<Aluno>(){
+            new Aluno(){
+                Id = 1,
+                Nome = "Marcos",
+                Sobrenome= "Gomes",
+                Telefone = "12345678"
+            },
+            new Aluno(){
+                Id = 2,
+                Nome = "Luis",
+                Sobrenome= "Hnerique",
+                Telefone = "12345678"
+            },
+            new Aluno(){
+                Id = 3,
+                Nome = "Silva",
+                Sobrenome= "Gadelha",
+                Telefone = "12345678"
+            }
+        };
         public AlunoController(){}
 
         [HttpGet]
         public IActionResult Get() 
         {
 
-            return Ok(" Deveriam haver alunos aqui");
+            return Ok(Alunos);
+        }
+
+        [HttpGet("ById")]
+        public IActionResult GetById(int Id) 
+        {
+            var aluno = Alunos.Find(a => Id == a.Id);
+
+            if (aluno == null) return StatusCode(StatusCodes.Status404NotFound);
+
+
+            return Ok(aluno);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome) 
+        { 
+            var aluno = Alunos.Find(a => nome == a.Nome);
+
+            if(aluno == null) return StatusCode(StatusCodes.Status404NotFound);
+
+            return Ok(aluno);
         }
 
     }
--- SmartSchoolAPI/Controllers/ProfessorController.cs
+++ SmartSchoolAPI/Controllers/ProfessorController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-//using SmartSchoolAPI.Models;
+using SmartSchoolAPI.Models;
 
 namespace SmartSchoolAPI.Controllers
 {
